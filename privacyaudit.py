85344b4ab3a28fdf7443e308053f6e54ce18e79f
jdiazbu1 <jeymyndiazb@gmail.com>
2026-04-14 16:20:48 -0400

    Initial docstrings & began draft for health insurance claim form

--- privacyaudit.py
+++ privacyaudit.py
@@ -1,1 +1,15 @@
+"""
+Python script for INST326 Final Project: Data Privacy Audit Tool. 
 
+Authors: Jeymy Diaz, Steve Donfack, Adam Marchello, Rugiatu R. Tarawally
+
+File Created: 14 April, 2026
+"""
+
+# Step 1:
+
+
+# Step 2:
+
+
+# Step 3: 
